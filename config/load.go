6ae1d2759f7ed27fc4c3d2ebe1aede8a87c78a87
hezixuan <hezixuan@52tt.com>
2024-07-04 18:21:16 +0800

    feat: update

--- config/load.go
+++ config/load.go
@@ -2,11 +2,11 @@ package config
 
 import (
 	_ "embed"
-	"encoding/json"
-	"fmt"
-	"os"
+	// "encoding/json"
+	// "fmt"
+	// "os"
 
-	"github.com/labstack/gommon/log"
+	// "github.com/labstack/gommon/log"
 )
 
 const ConfigFileName = "config.json"
@@ -15,42 +15,47 @@ const ConfigFileName = "config.json"
 var DefaultConfigJsonByte []byte
 
 func LoadConfig() (*Config, error) {
-	err := checkLocalConfigFile()
-	if err != nil {
-		return nil, err
-	}
+	// err := checkLocalConfigFile()
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	return loadLocalConfigFile()
+	// return loadLocalConfigFile()
+	c := &Config{Port: "6752", Debug:false, NotAllowedDeleteLog:false, RpcAddress: []*Address{}, CorsConfig: nil, StorageConfig: nil, MaxRoomNumber: 0, MaxLogFileSizeOfMB: 0, MaxLogLifeTimeOfHour: 0}
+	// {6752 false false [] <nil> <nil> 0 0 0})
+
+	return c, nil
 }
 
-func checkLocalConfigFile() error {
-	_, err := os.Stat(ConfigFileName)
-	if os.IsNotExist(err) {
-		log.Warnf("config file %s not exist", ConfigFileName)
-		file, err := os.Create(ConfigFileName)
-		if err != nil {
-			return fmt.Errorf("create config file %s error %w", ConfigFileName, err)
-		}
-		defer file.Close()
-		_, err = file.Write(DefaultConfigJsonByte)
-		if err != nil {
-			return fmt.Errorf("write config file %s error %w", ConfigFileName, err)
-		}
-	}
-	return nil
-}
+// func checkLocalConfigFile() error {
+// 	_, err := os.Stat(ConfigFileName)
+// 	if os.IsNotExist(err) {
+// 		log.Warnf("config file %s not exist", ConfigFileName)
+// 		file, err := os.Create(ConfigFileName)
+// 		if err != nil {
+// 			return fmt.Errorf("create config file %s error %w", ConfigFileName, err)
+// 		}
+// 		defer file.Close()
+// 		_, err = file.Write(DefaultConfigJsonByte)
+// 		if err != nil {
+// 			return fmt.Errorf("write config file %s error %w", ConfigFileName, err)
+// 		}
+// 	}
+// 	return nil
+// }
 
-func loadLocalConfigFile() (*Config, error) {
-	config := &Config{}
-	f, err := os.Open(ConfigFileName)
-	if err != nil {
-		return nil, fmt.Errorf("read config.json error %w", err)
-	}
-	defer f.Close()
-	encoder := json.NewDecoder(f)
-	err = encoder.Decode(config)
-	if err != nil {
-		return nil, fmt.Errorf("decode config.json error %w", err)
-	}
-	return config, nil
-}
+// func loadLocalConfigFile() (*Config, error) {
+// 	config := &Config{}
+// 	f, err := os.Open(ConfigFileName)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("read config.json error %w", err)
+// 	}
+// 	defer f.Close()
+// 	encoder := json.NewDecoder(f)
+// 	err = encoder.Decode(config)
+// 	if err != nil {
+// 		return nil, fmt.Errorf("decode config.json error %w", err)
+// 	}
+// 	log.Warnf("config file", config)
+// 	return config, nil
+// }
